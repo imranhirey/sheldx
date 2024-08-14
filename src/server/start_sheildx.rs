d45b250ff9d7c8325902ea9be60d80c77c3fc65d
laylabrimo <164578191+laylabrimo@users.noreply.github.com>
2024-08-14 21:32:25 +0100

    Update start_sheildx.rs

--- src/server/start_sheildx.rs
+++ src/server/start_sheildx.rs
@@ -8,11 +8,12 @@ use super::{ Server, WithoutTLS };
 pub async fn start_sheildx() -> Result<(), Box<dyn std::error::Error>> {
     let configs = load_configs()?;
     // log::debug!("ma ufaran yahay: {:?}", configs.is_tls_enabled);
-
+log::debug!("ma ufaran yahay: {:?}", configs.is_tls_enabled);
     if configs.is_tls_enabled {
 
         let server = WithTls {};
         server.start().await?;
+        
     } else {
         log::warn!("Sheldx recommends using TLS for production use");
         println!("Sheldx recommends using TLS for production use");
