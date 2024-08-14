2e73a638af8bae165bcac1713d9174462c453f82
imranhirey <imran@sheldx.com>
2024-08-14 23:53:03 +0100

    test dangerous feature

--- src/services/rate_limit.rs
+++ src/services/rate_limit.rs
@@ -12,10 +12,51 @@ strategy = "hashmap" // third look for this
 
 */
 
-// pub fn load_rate_limit_config() -> RateLimitConfig {
-//     let rate_limit_config = RateLimitConfig {
-//         limit: 100,
-//         duration: Duration::from_secs(60),
-//     };
-//     rate_limit_config
-// }
+
+pub fn enforce_rate_limit(
+    req: &Request<hyper::body::Body>,
+    client_ip: &str,
+    rate_limiter_map: &RateLimiterMap,
+) -> Result<(), ProxyError> {
+    let rate_limiters = rate_limiter_map.lock().unwrap();
+    let rate_limiter = rate_limiters
+        .get(client_ip)
+        .unwrap_or_else(|| {
+            let configs = load_configs().map_err(|_| ProxyError::ConfigError)?;
+            let rate_limit = configs.rate_limit.iter().find(|rate_limit| {
+                rate_limit.exclude.iter().all(|path| !req.uri().path().starts_with(path))
+            });
+
+            match rate_limit {
+                Some(rate_limit) => {
+                    let rate_limiter = Ratelimiter::builder(rate_limit.limit, Duration::from_secs(rate_limit.duration))
+                        .max_tokens(rate_limit.limit)
+                        .build()
+                        .unwrap();
+                    rate_limiter
+                }
+                None => {
+                    let rate_limiter = Ratelimiter::builder(10, Duration::from_secs(60))
+                        .max_tokens(10)
+                        .build()
+                        .unwrap();
+                    rate_limiter
+                }
+            }
+        });
+
+    // Apply rate limiting
+    if let Err(sleep) = rate_limiter.try_wait() {
+        // Rate limit exceeded
+
+        let title = "<h1>429 Too Many Requests</h1>";
+        let message = format!(
+            "<p>Rate limit exceeded. Try again in {} seconds</p>",
+            sleep.as_secs()
+        );
+        let response = show_html_page(title, &message);
+        return Err(ProxyError::HttpCommError);
+    }
+
+    Ok(())
+}
