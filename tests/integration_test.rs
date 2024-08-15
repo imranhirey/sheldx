960862e4c1095f99f4d75052271b3afbae34728d
imranhirey <imrannurhirey@gmail..com>
2024-08-15 02:38:29 +0100

    test has been setup

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,8 @@
+use std::fs;
+
 use hyper::{ body::Incoming, Request };
-use sheldx::{ handlers::show_html_page, utils::extract_host };
+use sheldx::{ handlers::show_html_page, utils::{create_default_config, extract_host} };
+use tokio::fs::File;
 
 #[test]
 fn should_show_default_page() {
@@ -12,3 +15,25 @@ fn should_show_default_page() {
 
 // configaration test
 
+// showuld craete sheldx folder in etc
+#[test]
+fn should_create_sheldx_folder() {
+    let response = create_default_config();
+    //check if returns Full<Bytes
+  // check if tehre is a folder called sheldx in etc
+    match fs::read_dir("/etc") {
+        Ok(dir) => {
+            let mut found = false;
+            for entry in dir {
+                if entry.unwrap().file_name() == "sheldx" {
+                    found = true;
+                    break;
+                }
+            }
+            assert_eq!(found, true);
+        }
+        Err(_) => {
+            assert_eq!(true, false);
+        }
+    }
+}
