f8fd06ed0fb874b27c4e40672a8cc2f4c3c1abd3
imranhirey <imrannurhirey@gmail..com>
2024-08-21 01:32:48 +0100

    added https - dev mode only

--- src/server/start.rs
+++ src/server/start.rs
@@ -97,7 +97,7 @@ impl Server for WithTLS {
 
     let configs = load_configs()?;
     let port = self.port.unwrap_or(PORTS::HTTPS as u16);
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 4444);
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 443);
 
     log::info!("Starting server on: {}", addr);
 
